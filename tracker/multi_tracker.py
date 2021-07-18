a4a1324b917f4c2e2e0ac06ecd4ce827c3da9c9b
toodamfilthy <dcfreire77@gmail.com>
2021-07-17 23:20:49 -0300

    Implemented MultiTracker
    It's interface is equivalent to OpenCV's, but can accept any tracker.
    https://docs.opencv.org/4.5.3/df/d4a/classcv_1_1legacy_1_1MultiTracker.html

---
+++ tracker/multi_tracker.py
@@ -0,0 +1,16 @@
+class MultiTracker:
+    def __init__(self):
+        self.trackers = []
+
+    def add(self, tracker, frame, roi):
+        tracker.init(frame, roi)
+        self.trackers.append((tracker, frame))
+    
+    def update(self, frame):
+        rois = []
+        for tracker, frame in self.trackers:
+            _, roi = tracker.update(frame)
+            rois.append(roi)
+        return rois
+
+    
