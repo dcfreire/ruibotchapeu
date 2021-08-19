948db5d6c2f01ef334f428425647b016fa9526f8
Daniel Freire <daniel.carneiro.freire@gmail.com>
2021-08-19 20:19:26 -0300

    Changed number of clusters to 1

--- utils/video.py
+++ utils/video.py
@@ -4,6 +4,5 @@ def saveVideo(frames):
     fourcc = cv2.VideoWriter_fourcc(*'FMP4')
     out = cv2.VideoWriter('output.mp4', fourcc, 30, (frames[0].shape[1], frames[0].shape[0]))
     for frame in frames:
-        print(ok)
         out.write(frame)
     out.release()
